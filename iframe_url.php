842a3e313381d272de8c5e999e9a19e70a87fb2f
BinqiangLiu <134038370+BinqiangLiu@users.noreply.github.com>
2023-12-09 19:13:39 +0800

    Update iframe_url.php

--- iframe_url.php
+++ iframe_url.php
@@ -1,9 +1,7 @@
-<?
+<?php
 $iframe_url = "https://sustech-sus-chat-34b.hf.space/";
-
 $Referer = @$_SERVER["HTTP_REFERER"];
 $RefererHost = @explode(":", explode("/", explode("//", $Referer)[1])[0])[0];
-
 if ($RefererHost == $_SERVER["SERVER_NAME"]) {
     header("Location: " . $iframe_url);
 } else {
